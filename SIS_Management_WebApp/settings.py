8209a4577b990ed27c37f3e8d35f49e9841a48f8
09196813 <rofl001@ess.ais.ac.nz>
2024-07-13 14:12:11 +1200

    updated settings with with cors header in installed app

--- SIS_Management_WebApp/settings.py
+++ SIS_Management_WebApp/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'management_webapp',
-    'corseheaders',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -144,12 +144,12 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
 
-    'http://localhost:3000',
+    'http://localhost:8000',
 
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
 
-    'http://localhost:3000',
+    'http://localhost:8000',
 
 ]
